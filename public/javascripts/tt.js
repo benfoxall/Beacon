a78c67db5cef78c9637992fff552a731356138fc
Ben Foxall <benfoxall@gmail.com>
2011-05-14 08:24:55 +0100

    reformatted some files - made it work on different hosts

--- public/javascripts/tt.js
+++ public/javascripts/tt.js
@@ -1,22 +1,19 @@
 $(function(){
+    var host = window.location.host;
+    
+    $('.host').text(host);
+
     $('input#key').keyup(function(event){
         var k = $('input').val().split(/[^A-z0-9]/).join('');
         $('.k').text(k);
         $('.keylink').attr('href','/tt/' + k)
     }).focus();
-    
+
     if(window.ttKey){
-        // alert("load")
-        var client = new Faye.Client('http://localhost:3000/faye');
+        var client = new Faye.Client('http://'+host+'/faye');
         client.subscribe('/' + ttKey, function(message) {
             $('#tt').append($('<li>').text(message.text))
-          alert('Got a message: ' + message.text);
+            alert('Got a message: ' + message.text);
         });
-        
-        console.log("Subscribed", '/' + ttKey);
-    }
-    
-    if($('#tt').size() > 0){
-        $('#tt').append('<li>ok</li>')
     }
 })
